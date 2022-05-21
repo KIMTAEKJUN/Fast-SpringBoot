1ce9f354c6461879245efe894b46296d70ce8bac
KimTaekJun <86834898+Kuah0@users.noreply.github.com>
2022-05-21 19:35:06 +0900

    파일 대 이동

---
+++ spring-boot-start/src/response/src/test/java/com/example/response/ResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package response.src.test.java.com.example.response;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
