6fa12b0d3278bc63a4bbb9a382bb928a63290e45
KimTaekJun <86834898+Kuah0@users.noreply.github.com>
2022-05-23 15:41:28 +0900

    IoC / DI Studying . . .

---
+++ spring-boot-middle/src/spring-ioc/src/main/java/com/example/springioc/SpringIocApplication.java
@@ -0,0 +1,13 @@
+package com.example.springioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringIocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocApplication.class, args);
+	}
+}
